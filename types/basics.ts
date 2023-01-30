fc1499ae28bb55381e48d12cf49a0744d79089b3
enesyesilkutuk <enesyesilkutuk@gmail.com>
2023-01-30 19:10:19 +0300

    new notes added

---
+++ types/basics.ts
@@ -0,0 +1,22 @@
+console.log("Murat");
+
+// const button = document.querySelector('button')! as HTMLButtonElement;
+
+// button.addEventListener('click', () => console.log("Button was clicked"));
+
+// tsc app.ts
+
+// const add = (a:number, b:number, c: boolean) => {
+//     if (c)  return a + b;
+//     alert("sdfghjk")
+// };
+
+// const num1 = 13;
+// const num2 = 35;
+// const go = true;
+
+// const res = add(num1, num2, go);
+// console.log(res);
+
+
+
---
+++ types/objects.ts
@@ -0,0 +1,82 @@
+// How to Define an object
+
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+    name: "Enes",
+    age: 32,
+    hobbies: ["football"],
+    role: [12, "driver"],
+};
+
+console.log(person.name); 
+console.log(person.age);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+};
+
+// How to Define a nested object
+
+const product: {
+    id: string;
+    price: number;
+    tags: string[];
+    details: {
+        title: string;
+        description: string;
+    }
+} = {
+    id: '1',
+    price: 123,
+    tags: ['super', "excellent"],
+    details: {
+        title: "pencil",
+        description: "good",
+    }
+};
+
+// Working with enums
+
+enum Role {
+    ADMIN, READ_ONLY, AUTHOR
+};
+
+// ADMIN : 0, READ_ONLY : 1, AUTHOR : 2
+
+enum Status {
+    MANAGER = 23,
+    DEVELOPER,
+    BOSS,
+};
+
+// MANAGER: 23, DEVELOPER: 24, BOSS: 25
+
+enum Position {
+    FRONTEND = "FRONTEND", BACKEND = 451, FULL_STACK = 916
+};
+
+// console.log(Position.BACKEND); 451
+
+const man = {
+    name: "ali",
+    age: 21,
+    hobbies: ["theatre", "basketball"],
+    role: Role.ADMIN,
+};
+
+const member: {
+    name: string;
+    age: number;
+    hobbies: (string | number)[];
+    role: [number, string];
+} = {
+    name: "Enes",
+    age: 32,
+    hobbies: ["football", 15, "swimming"],
+    role: [90, "asd"],
+};
+
